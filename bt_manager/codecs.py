8d46dfea51dab05b9c5aa63460847665aff44ca4
John Damits <kakoijohn@gmail.com>
2020-03-24 19:07:26 -0400

    Update codecs.py

--- bt_manager/codecs.py
+++ bt_manager/codecs.py
@@ -179,7 +179,7 @@ class SBCCodec:
                                         self.seq_num,
                                         fd)
 
-    def decode(self, fd, mtu, max_len=64):
+    def decode(self, fd, mtu, max_len=8):
         """
         Read the media transport descriptor, depay
         the RTP payload and decode the SBC frames into
